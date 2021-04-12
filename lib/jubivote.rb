0bad94dadf8d46d36ea2006ddf7e25bb9b5651be
Justin Bishop <jubishop@gmail.com>
2021-04-11 18:31:51 -0700

    rubocop

--- lib/jubivote.rb
+++ lib/jubivote.rb
@@ -29,7 +29,8 @@ module JubiVote
       from = SendGrid::Email.new(email: 'jubivote@jubishop.com')
       to = SendGrid::Email.new(email: 'jubi@hey.com')
       subject = 'Sending with Twilio SendGrid is Fun'
-      content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+      content = SendGrid::Content.new(type: 'text/plain',
+                                      value: 'and easy to do anywhere')
       mail = SendGrid::Mail.new(from, subject, to, content)
 
       sg = SendGrid::API.new(api_key: ENV.fetch('SENDGRID_API_KEY'))
