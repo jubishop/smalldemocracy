93f5622fbbc841e8c762e4aea412908cba0ec1a6
Justin Bishop <jubishop@gmail.com>
2021-04-11 17:56:50 -0700

    testing sendgrixg

--- lib/jubivote.rb
+++ lib/jubivote.rb
@@ -23,7 +23,20 @@ module JubiVote
     }
 
     post('/admin/create_poll') {
-      puts 'posting creation of poll'
+      require 'sendgrid-ruby'
+      include SendGrid
+
+      from = SendGrid::Email.new(email: 'jubivote@jubishop.com')
+      to = SendGrid::Email.new(email: 'jubi@hey.com')
+      subject = 'Sending with Twilio SendGrid is Fun'
+      content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+      mail = SendGrid::Mail.new(from, subject, to, content)
+
+      sg = SendGrid::API.new(api_key: ENV.fetch('SENDGRID_API_KEY'))
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+      puts response.status_code
+      puts response.body
+      puts response.headers
     }
   end
 end
