d11d6f51424b36e414122b98d5579e0d5ba4a246
Justin Bishop <jubishop@gmail.com>
2021-04-13 21:36:12 -0700

    cleaner eager call

--- lib/models/poll.rb
+++ lib/models/poll.rb
@@ -5,7 +5,7 @@ require_relative 'choice'
 class Poll < Sequel::Model
   unrestrict_primary_key
   one_to_many :choices
-  eager(:choices)
+  eager :choices
 
   def initialize(**args)
     args[:id] = SecureRandom.alphanumeric(16)
