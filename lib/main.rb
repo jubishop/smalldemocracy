64b56b75478274abdc23f3069e8b46efb1e1a5f6
Justin Bishop <jubishop@gmail.com>
2021-04-20 18:28:50 -0700

    more tests

--- lib/main.rb
+++ lib/main.rb
@@ -7,6 +7,10 @@ require_relative 'utils/email'
 class Main < Base
   get('/') {
     puts request.get_header('HTTP_X_FORWARDED_PROTO')
+    puts request.get_header('HTTP_FLY_FORWARDED_PROTO')
+    puts request.get_header('HTTP_FLY_FORWARDED_SSL')
+    puts request.get_header('HTTP_X_FORWARDED_SSL')
+    puts request.get_header('HTTP_FLY_FORWARDED_PORT')
     slim :index, locals: { email: fetch_email }
   }
 
