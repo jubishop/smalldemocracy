e067ffbb4a44dc2e7b87c7ccd34ddaa1c6f8716e
Justin Bishop <jubishop@gmail.com>
2021-04-20 21:00:40 -0700

    more robust redirect test

--- spec/full_stack_spec.rb
+++ spec/full_stack_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe('Full stack config.ru') {
   it('redirects to https in production only') {
     ENV['RACK_ENV'] = 'production'
     get '/', nil, { FLY_FORWARDED_PROTO: 'http' }
+
+    expect(last_request.ssl?).to(be(false))
     expect(last_response.redirect?).to(be(true))
+
+    follow_redirect!
+    expect(last_request.ssl?).to(be(true))
   }
 }
