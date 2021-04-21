bbf802b38a6461525e45933f01520f043c5fb781
Justin Bishop <jubishop@gmail.com>
2021-04-20 23:06:46 -0700

    more tests!

--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -17,13 +17,16 @@ RSpec.describe(Poll, type: :feature) {
       fill_in 'question', with: 'what is life'
       fill_in 'responders', with: email
       fill_in 'choices', with: 'one, two, three'
-      fill_in 'expiration', with: '1243121441124'
+      fill_in 'expiration', with: Time.now.to_i + 90
       click_button 'Submit'
 
+      expect(page).to(have_selector('h1', exact_text: 'this is my title'))
+      expect(page).to(have_selector('h2', exact_text: 'what is life'))
       expect(page).to(have_selector('.choice', count: 3))
-      expect(page).to(have_selector('.choice', count: 1, text: 'one'))
-      expect(page).to(have_selector('.choice', count: 1, text: 'two'))
-      expect(page).to(have_selector('.choice', count: 1, text: 'three'))
+      expect(page).to(have_selector('.choice', count: 1, exact_text: 'one'))
+      expect(page).to(have_selector('.choice', count: 1, exact_text: 'two'))
+      expect(page).to(have_selector('.choice', count: 1, exact_text: 'three'))
+      expect(page).to(have_selector('p', text: '1 minute from now'))
     }
   }
 }
