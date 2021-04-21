54a63b6838cca3bbdd367209fb0757f5ba0f6311
Justin Bishop <jubishop@gmail.com>
2021-04-20 18:27:08 -0700

    try again

--- lib/main.rb
+++ lib/main.rb
@@ -6,7 +6,7 @@ require_relative 'utils/email'
 
 class Main < Base
   get('/') {
-    puts request.get_header('rack.url_scheme')
+    puts request.get_header('HTTP_X_FORWARDED_PROTO')
     slim :index, locals: { email: fetch_email }
   }
 
