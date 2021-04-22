9e04cb6f33500d800a686fac85bd8f3a733f95aa
Justin Bishop <jubishop@gmail.com>
2021-04-22 00:13:09 -0700

    prettier syntax

--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -3,8 +3,8 @@ require_relative '../../lib/helpers/cookie'
 module RSpec
   module Session
     def fake_email_cookie(email = 'test@example.com')
-      allow_any_instance_of(Helpers::Cookie).to(
-          receive(:fetch_email).and_return(email))
+      allow_any_instance_of(Helpers::Cookie).to(receive(:fetch_email)
+                                            .and_return(email))
       return email
     end
   end
