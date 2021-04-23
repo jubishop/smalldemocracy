d4bd92b038034a7a7f16f0464064aa0d90b97de3
Justin Bishop <jubishop@gmail.com>
2021-04-22 20:15:53 -0700

    fleshed out poll model compute results test

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../../lib/models/poll'
 
 RSpec.describe(Models::Poll) {
-  context('::create_poll') {
-    let(:basic_options) {
-      {
-        title: 'title',
-        question: 'question',
-        expiration: 123
-      }
+  let(:basic_options) {
+    {
+      title: 'title',
+      question: 'question',
+      expiration: 123
     }
+  }
 
+  context('::create_poll') {
     it('creates a poll with comma strings') {
       poll = Models::Poll.create_poll(**basic_options,
                                       choices: 'one, two, three',
@@ -24,6 +24,46 @@ RSpec.describe(Models::Poll) {
                                       responders: ['d@e', 'e@f', 'f@g'])
       expect(poll.choices.map(&:text)).to(match_array(%w[four five six]))
       expect(poll.responders.map(&:email)).to(match_array(%w[d@e e@f f@g]))
+    }
+  }
+
+  context('#results') {
+    it('returns no results if the poll is not expired') {
+      allow(Time).to(receive(:now).and_return(Time.at(0)))
+      poll = Models::Poll.create_poll(**basic_options,
+                                      choices: [],
+                                      responders: [])
+      expect(poll.results).to(be_falsey)
+    }
+
+    it('computes results properly') {
+      choices = %w[one two three four]
+      responders = %w[a b c d]
+      allow(Time).to(receive(:now).and_return(Time.now + 100))
+      poll = Models::Poll.create_poll(**basic_options,
+                                      choices: choices,
+                                      responders: responders)
+
+      responses = {
+        a: %w[one two three four],
+        b: %w[one two four three],
+        c: %w[three one two four],
+        d: %w[four two three one]
+      }
+      responses.each { |email, ranks|
+        responder = poll.responder(email: email.to_s)
+        poll.choices.each { |choice|
+          responder.add_response(choice_id: choice.id,
+                                 rank: ranks.index(choice.text))
+        }
+      }
+
+      results = { one: 8, two: 7, three: 5, four: 4 }
+      results.each_with_index { |result, index|
+        choice, score = *result
+        expect(poll.results[index].text).to(eq(choice.to_s))
+        expect(poll.results[index].score).to(eq(score))
+      }
     }
   }
 }
--- spec/rack/poll_spec.rb
+++ spec/rack/poll_spec.rb
@@ -1,21 +1,26 @@
 RSpec.describe('/poll', type: :feature) {
   include_context(:apparition)
 
+  def create_poll(email:, time:)
+    visit('/poll/create')
+    fill_in('title', with: 'this is my title')
+    fill_in('question', with: 'what is life')
+    fill_in('responders', with: email)
+    fill_in('choices', with: 'one, two, three')
+    fill_in('expiration', with: time)
+    click_button('Submit')
+  end
+
   context('/create') {
-    it('makes a poll') {
+    it('kicks off a full poll lifecycle') {
       current_time = 388341770
       allow(Time).to(receive(:now).and_return(Time.at(current_time)))
 
+      # Create a poll
       page.set_cookie(:email, 'test@example.com')
-      visit '/poll/create'
+      create_poll(email: 'test@example.com', time: current_time + 61)
 
-      fill_in 'title', with: 'this is my title'
-      fill_in 'question', with: 'what is life'
-      fill_in 'responders', with: 'test@example.com'
-      fill_in 'choices', with: 'one, two, three'
-      fill_in 'expiration', with: current_time + 61
-      click_button 'Submit'
-
+      # Fill in poll choices
       expect(page).to(have_fontawesome)
       expect(page).to(have_selector('h1', exact_text: 'this is my title'))
       expect(page).to(have_selector('h2', exact_text: 'what is life'))
@@ -28,9 +33,11 @@ RSpec.describe('/poll', type: :feature) {
       RSpec::Goldens.verify(page, 'poll_respond', full: true)
       click_button 'Submit Choices'
 
+      # See recorded responses
       expect(page).to(have_content('recorded responses'))
       RSpec::Goldens.verify(page, 'poll_responded', full: true)
 
+      # See finished poll results
       allow(Time).to(receive(:now).and_return(Time.at(current_time + 62)))
       refresh
       expect(page).to(have_content('finished'))
