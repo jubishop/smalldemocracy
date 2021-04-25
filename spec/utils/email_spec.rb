5f453b57b4c37d7d53f2ff9ae564f011c4e1626f
Justin Bishop <jubishop@gmail.com>
2021-04-24 22:43:33 -0700

    much smarter assertion about content of emails

--- spec/utils/email_spec.rb
+++ spec/utils/email_spec.rb
@@ -16,13 +16,12 @@ RSpec.describe(Utils::Email) {
     poll = create_poll(responders: 'jubi@hey.com')
     allow(Process).to(receive(:detach))
     allow(Process).to(receive(:fork)) { |&arg| arg.call }
-    # rubocop:disable Style/StringHashKeys
-    expect_any_instance_of(SendGrid::Client).to(receive(:post)).once.with(
-        hash_including(request_body: hash_including(
-            'content' => [
-              hash_including('value' => a_string_matching(poll.id))
-            ])))
-    # rubocop:enable Style/StringHashKeys
+    expect_any_instance_of(SendGrid::Client).to(receive(:post)).once { |_, data|
+      node = Capybara.string(data[:request_body]['content'].first['value'])
+      base = 'https://www.jubivote.com/poll/view/'
+      query = "#{poll.id}?responder=#{poll.responders.first.salt}"
+      expect(node).to(have_link('click here', href: "#{base}#{query}"))
+    }
     Utils::Email.email(poll, poll.responders.first)
   }
 }
