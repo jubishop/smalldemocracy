6e37506939a463468c306dbba39ef97bdecdb364
Justin Bishop <jubishop@gmail.com>
2021-04-29 16:10:06 -0700

    apparition full lifecycle test of borda_split polls

--- spec/apparition/poll_spec.rb
+++ spec/apparition/poll_spec.rb
@@ -1,34 +1,57 @@
 RSpec.describe(Poll, type: :feature) {
-  it('supports full poll lifecycle') {
-    # It's 1982!
-    current_time = 388341770
-    allow(Time).to(receive(:now).and_return(Time.at(current_time)))
+  context('full poll lifecycles') {
+    def submit_creation(page_name)
+      find('h1').click # Deselect any form field
+      RSpec::Goldens.verify(page, page_name, full: true)
+      click_button('Submit')
+    end
 
-    # Create a poll
-    page.set_cookie(:email, 'test@example.com')
-    visit('/poll/create')
-    fill_in('title', with: 'this is my title')
-    fill_in('question', with: 'what is life')
-    fill_in('responders', with: 'test@example.com')
-    fill_in('choices', with: 'one, two, three')
-    fill_in('expiration', with: current_time + 61)
-    find('h1').click # Deselect any form field
-    RSpec::Goldens.verify(page, 'poll_create', full: true)
-    click_button('Submit')
+    def submit_choices(page_name)
+      expect(page).to(have_fontawesome)
+      expect(page).to(have_button(text: 'Submit Choices'))
+      RSpec::Goldens.verify(page, page_name, full: true)
+      click_button('Submit Choices')
+    end
 
-    # Submit choices
-    expect(page).to(have_fontawesome)
-    expect(page).to(have_button(text: 'Submit Choices'))
-    RSpec::Goldens.verify(page, 'poll_view', full: true)
-    click_button 'Submit Choices'
+    def verify_finished_poll(page_name)
+      allow(Time).to(receive(:now).and_return(Time.at(10**10)))
+      refresh
+      RSpec::Goldens.verify(page, page_name, full: true)
+    end
 
-    # See recorded responses
-    RSpec::Goldens.verify(page, 'poll_responded', full: true)
+    before(:each) {
+      # It's 1982!
+      current_time = 388341770
+      allow(Time).to(receive(:now).and_return(Time.at(current_time)))
 
-    # See finished poll results
-    allow(Time).to(receive(:now).and_return(Time.at(10**10)))
-    refresh
-    RSpec::Goldens.verify(page, 'poll_finished', full: true)
+      # Create a poll
+      page.set_cookie(:email, 'test@example.com')
+      visit('/poll/create')
+      fill_in('title', with: 'this is my title')
+      fill_in('question', with: 'what is life')
+      fill_in('responders', with: 'test@example.com')
+      fill_in('choices', with: 'one, two, three')
+      fill_in('expiration', with: current_time + 61)
+    }
+
+    it('executes borda_single') {
+      submit_creation('poll_borda_single_create')
+      submit_choices('poll_borda_single_view')
+      RSpec::Goldens.verify(page, 'poll_borda_single_responded', full: true)
+      verify_finished_poll('poll_borda_single_finished')
+    }
+
+    it('executes borda_split') {
+      select('Borda Split', from: 'type')
+      submit_creation('poll_borda_split_create')
+
+      # TODO: Drag something to the red box.
+      page.first('li.choice').drag_to(page.find_by_id('bottom-choices'))
+      submit_choices('poll_borda_split_view')
+
+      RSpec::Goldens.verify(page, 'poll_borda_split_responded', full: true)
+      verify_finished_poll('poll_borda_split_finished')
+    }
   }
 
   context('poll') {
