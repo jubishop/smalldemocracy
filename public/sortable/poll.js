0f2739ee927aba966522420a30564f49b2fe0554
Justin Bishop <jubishop@gmail.com>
2021-04-29 20:23:05 -0700

    auto updating points

--- public/sortable/poll.js
+++ public/sortable/poll.js
@@ -1,12 +1,21 @@
 class Poll {
   static domLoaded() {
     const choicesElement = document.getElementById('choices');
-    const bottomChoicesElement = document.getElementById('bottom-choices')
+    const bottomChoicesElement = document.getElementById('bottom-choices');
+
+    const choicesArray = Array.from(document.getElementsByClassName('choice'));
+    this.choiceElements = Object.fromEntries(choicesArray.map((choice) => {
+      return [
+        choice.getAttribute('data-id'),
+        choice.getElementsByClassName('score')[0]
+      ];
+    }));
 
     const options = {
       animation: 100,
-      group: 'choices'
-    }
+      group: 'choices',
+      onSort: () => this.updateScores()
+    };
     if(window.matchMedia("(pointer: coarse)").matches) {
       options.handle = '.grip';
     }
@@ -15,12 +24,32 @@ class Poll {
       this.bottomSortable = Sortable.create(bottomChoicesElement, options);
     }
 
+    this.updateScores();
+
     this.pollID = choicesElement.getAttribute('poll_id');
     this.responderSalt = choicesElement.getAttribute('responder_salt');
 
     this.submitButton = document.getElementById('submit');
     this.submitButton.addEventListener('click', () => this.submitClicked());
     this.submitButton.disabled = false;
+  }
+
+  static updateScores() {
+    let baseScore = 0;
+    if (this.bottomSortable) {
+      baseScore = this.bottomSortable.toArray().length;
+      this.bottomSortable.toArray().forEach((choiceID) => {
+        this.choiceElements[choiceID].textContent = '0 points';
+      });
+    }
+
+    let topScore = this.sortable.toArray().length - 1;
+    this.sortable.toArray().forEach((choiceID) => {
+      const currentScore = topScore + baseScore;
+      topScore -= 1;
+      const points = currentScore == 1 ? 'point' : 'points';
+      this.choiceElements[choiceID].textContent = currentScore + ' ' + points;
+    });
   }
 
   static async submitClicked() {
@@ -33,7 +62,7 @@ class Poll {
         poll_id: this.pollID,
         responder: this.responderSalt,
         responses: this.sortable.toArray(),
-        bottom_responses: this?.bottomSortable?.toArray(),
+        bottom_responses: this?.bottomSortable?.toArray()
       }),
       headers: { 'Content-Type': 'application/json' }
     }).then(res => {
