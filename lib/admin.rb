69950faebc32ae3658bee3f9f3919afbdb736940
Justin Bishop <jubishop@gmail.com>
2021-12-16 18:22:51 -0800

    clear out most admin stuff, just leaving landing page
    resolves #123

--- lib/admin.rb
+++ lib/admin.rb
@@ -9,30 +9,5 @@ class Admin < Base
     get('/admin', ->(_, resp) {
       resp.write(@slim.render('admin/admin'))
     })
-
-    get('/admin/poll/create', ->(_, resp) {
-      resp.write(@slim.render('/poll/create'))
-    })
-
-    post('/admin/poll/create', ->(req, resp) {
-      poll = Models::Poll.create(**req.params.to_h.symbolize_keys)
-      resp.redirect("/admin#{poll.url}")
-    })
-
-    get(%r{^/admin/poll/view/(?<poll_id>.+)$}, ->(req, resp) {
-      poll = require_poll(req, resp)
-
-      resp.write(@slim.render('admin/view_poll', poll: poll))
-    })
-
-    get('/admin/poll/blast', ->(req, resp) {
-      poll = require_poll(req, resp)
-
-      poll.responders.each { |responder|
-        Utils::Email.email(poll, responder)
-      }
-
-      resp.write(slim.render('admin/blast_emails_sent', poll: poll))
-    })
   end
 end
