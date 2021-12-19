e6709a7b37244dbed97df56d484265b1f4142e5e
Justin Bishop <jubishop@gmail.com>
2021-12-18 23:27:06 -0800

    migrate to using pico.css. much needs to be fixed about coloring to finalize new goldens.

--- spec/helpers/poll/expectations.rb
+++ spec/helpers/poll/expectations.rb
@@ -24,19 +24,21 @@ module RSpec
 
     def expect_borda_single_finished_page
       expect(last_response.ok?).to(be(true))
-      expect(last_response.body).to(have_content('is finished'))
+      expect(last_response.body).to(have_content('Results:'))
       expect(last_response.body).to(have_selector('ol', count: 1))
+      expect(last_response.body).to(have_selector('h2', text: 'Unresponded'))
     end
 
     def expect_borda_split_finished_page
       expect(last_response.ok?).to(be(true))
-      expect(last_response.body).to(have_content('is finished'))
+      expect(last_response.body).to(have_content('Results:'))
       expect(last_response.body).to(have_selector('ol', count: 2))
+      expect(last_response.body).to(have_selector('h2', text: 'Unresponded'))
     end
 
     def expect_choose_one_finished_page
       expect(last_response.ok?).to(be(true))
-      expect(last_response.body).to(have_content('is finished'))
+      expect(last_response.body).to(have_content('Results:'))
       expect(last_response.body).to(have_selector('h2', text: 'Unresponded'))
     end
 
