de0d22304a3b6419333dfeadec3003f5e10a88df
Justin Bishop <jubishop@gmail.com>
2021-12-25 00:05:49 -0800

    more Time refining

--- spec/helpers/time.rb
+++ spec/helpers/time.rb
@@ -1,15 +1,19 @@
 module RSpec
   module Time
     def future
-      return ::Time.now + 10
+      return ::Time.now + 99
     end
 
     def past
-      return ::Time.now - 10
+      return ::Time.now - 99
+    end
+
+    def random_time
+      return ::Time.at(rand(::Time.now.to_i))
     end
 
     def freeze_time
-      moment = ::Time.at(rand(::Time.now.to_i))
+      moment = random_time
       allow(::Time).to(receive(:now).and_return(moment))
       return moment
     end
