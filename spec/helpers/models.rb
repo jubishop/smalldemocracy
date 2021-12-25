f50a1409f444d040be741fc962a9dac2cc48b702
Justin Bishop <jubishop@gmail.com>
2021-12-25 00:10:46 -0800

    get rid of mock_response

--- spec/helpers/models.rb
+++ spec/helpers/models.rb
@@ -121,26 +121,6 @@ module Models
       test_only!
       orig_add_choice.bind_call(self, text: text)
     }
-
-    def mock_response
-      test_only!
-      responder = responder(email: 'a@a')
-      responses = choices.map(&:id)
-
-      case type
-      when :borda_single, :borda_split
-        responses.each_with_index { |choice_id, rank|
-          score = responses.length - rank
-          score -= 1 if type == :borda_single
-
-          responder.add_response(choice_id: choice_id, score: score)
-        }
-      when :choose_one
-        responder.add_response(choice_id: choices.first.id)
-      end
-
-      return responses
-    end
   end
 
   class Choice
