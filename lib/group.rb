0d41001ae5bc5cdbdac86ac4b7fdcaecdae3032b
Justin Bishop <jubishop@gmail.com>
2022-01-06 20:03:15 -0800

    better group endpoint names

--- lib/group.rb
+++ lib/group.rb
@@ -39,10 +39,10 @@ class Group < Base
       return 200, @slim.render('group/view', group: group, member: member)
     })
 
-    post('/group/add', ->(req, _) {
+    post('/group/add_member', ->(req, _) {
     })
 
-    post('/group/remove', ->(req, _) {
+    post('/group/remove_member', ->(req, _) {
     })
   end
 end
