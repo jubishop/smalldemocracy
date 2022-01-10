bf3310d35b69d12e3b21ae123aba709158a67fd5
Justin Bishop <jubishop@gmail.com>
2022-01-10 14:56:12 -0800

    use Object.assign() for cleaner and more readable default options for Listable class

--- src/lib/listable.js
+++ src/lib/listable.js
@@ -4,13 +4,16 @@ class Listable {
   constructor(listElement, buttonElement, options = {}) {
     this.listElement = listElement;
     this.buttonElement = buttonElement;
-    this.options = options;
+    this.options = Object.assign({
+      deleteButtonClasses: ['delete-button', 'fa-duotone', 'fa-trash-can'],
+      inputName: 'item[]',
+      inputType: 'text',
+      listItemClasses: ['listable'],
+      placeholderText: '',
+      textClasses: ['text']
+    }, options);
     this.items = [];
     this.buttonElement.addEventListener('click', () => this.addItem());
-  }
-
-  option(key, fallback = '') {
-    return this.options.hasOwnProperty(key) ? this.options[key] : fallback;
   }
 
   addItem() {
@@ -43,17 +46,17 @@ class Listable {
 
   buildListItem() {
     const listItem = document.createElement('li');
-    listItem.classList.add(...this.option('listItemClasses', ['listable']));
+    listItem.classList.add(...this.options['listItemClasses']);
     return listItem;
   }
 
   buildInputElement() {
     const inputElement = document.createElement('input');
-    inputElement.classList.add(...this.option('textClasses', ['text']));
-    inputElement.setAttribute('type', this.option('inputType', 'text'));
-    inputElement.setAttribute('name', this.option('inputName', 'item[]'))
+    inputElement.classList.add(...this.options['textClasses']);
+    inputElement.setAttribute('type', this.options['inputType']);
+    inputElement.setAttribute('name', this.options['inputName']);
     inputElement.setAttribute('required', true);
-    inputElement.setAttribute('placeholder', this.option('placeholderText'));
+    inputElement.setAttribute('placeholder', this.options['placeholderText']);
     inputElement.addEventListener('keydown', (event) => {
       if (event.key == "Enter") {
         event.preventDefault();
@@ -73,9 +76,7 @@ class Listable {
   buildDeleteButton() {
     const deleteButton = document.createElement('div');
     const deleteIcon = document.createElement('i');
-    deleteIcon.classList.add(
-      ...this.option('deleteButtonClasses',
-        ['delete-button', 'fa-duotone', 'fa-trash-can']));
+    deleteIcon.classList.add(...this.options['deleteButtonClasses'])
     deleteButton.appendChild(deleteIcon);
     return deleteButton;
   }
