e663543cd2d5cbaf269e1106c779fe234959dadf
Justin Bishop <jubishop@gmail.com>
2022-01-10 16:08:32 -0800

    making js progress towards group editability

--- src/group/view.js
+++ src/group/view.js
@@ -2,7 +2,24 @@ import { Editable } from '../lib/editable'
 
 class Group {
   static domLoaded() {
-    new Editable();
+    const editableElementXPath = document.evaluate(
+      "//li[@class='editable' and ./div[@class='deletable']]", document);
+    const editableElements = [];
+    let editableElement = editableElementXPath.iterateNext();
+    while (editableElement) {
+      editableElements.push(editableElement);
+      editableElement = editableElementXPath.iterateNext()
+    }
+    new Editable(
+      '/group/add_member',
+      '/group/delete_member',
+      editableElements,
+      (elementToAdd) => {
+        return 'hello from add';
+      },
+      (elementToDelete) => {
+        return 'hello from delete';
+      });
   }
 }
 
--- src/lib/editable.js
+++ src/lib/editable.js
@@ -1,5 +1,29 @@
 export { Editable }
 
 class Editable {
-  constructor() { }
+  constructor(addPath,
+              deletePath,
+              editableElements,
+              addCallback,
+              deleteCallback,
+              options = {}) {
+    this.addPath = addPath;
+    this.deletePath = deletePath;
+    this.elements = editableElements;
+    this.addCallback = addCallback;
+    this.deleteCallback = deleteCallback;
+    this.options = Object.assign({
+      deleteContainerClass: 'deletable'
+    }, options);
+    for (const element of this.elements) {
+      const deleteElement = element.getElementsByClassName(
+        this.options['deleteContainerClass'])[0];
+      deleteElement.addEventListener('click',
+        () => this.deleteItem(element));
+    }
+  }
+
+  deleteItem(element) {
+    console.log(this.deleteCallback(element));
+  }
 }
