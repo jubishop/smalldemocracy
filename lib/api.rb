6e883f80a7ef97ef90c152f4bb9b937147169f41
Justin Bishop <jubishop@gmail.com>
2022-04-20 22:37:40 -0700

    simple endpoint proves taking key (in json or url params format) and return user

--- lib/api.rb
+++ lib/api.rb
@@ -24,5 +24,19 @@ class API < Base
       user.update(api_key: Models::User.create_api_key)
       return 201, user.api_key
     })
+
+    post('/api/poll/new', ->(req, _resp) {
+      user = require_key(req)
+      return 201, user.email
+    })
+  end
+
+  private
+
+  def require_key(req)
+    key = req.param(:key)
+    user = Models::User.find(api_key: key)
+    throw(:response, [401, 'Invalid key']) unless user
+    return user
   end
 end
